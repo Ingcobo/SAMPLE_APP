eddcbdd1decc8056dc3982312a2f272f56c2c8f2
Innocent Ngcobo <ingcobo064@gmail.com>
2017-03-15 18:36:29 +0000

    Finish user signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,2 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-
-  class User
-  attr_accessor :name, :email
-
-  def initialize(attributes = {})
-    @name  = attributes[:name]
-    @email = attributes[:email]
-  end
-
-  def formatted_email
-    "#{@name} <#{@email}>"
-  end
-  end
 end
